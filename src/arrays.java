2ff549fec8f71cfd3d542cb066742f522342bd10
dilipgour <codewithdilipgour@gmail.com>
2026-02-28 21:49:57 +0530

    Day-12

--- src/arrays.java
+++ src/arrays.java
@@ -90,10 +90,17 @@ void main(){
 
     int[][] matanas = generateMatrix(3);
     matanas=spiralMatrixIII(5,6,1,4);
-    for (int i = 0; i < matanas.length; i++) {
-        System.out.println(Arrays.toString(matanas[i]));
-    }
+//    for (int i = 0; i < matanas.length; i++) {
+//        System.out.println(Arrays.toString(matanas[i]));
+//    }
 
+    int[][] mat = new int[][]{{5,1,0},
+                               {3,4,5},
+                               {6,7,8}};
+    setZeroes(mat);
+    for (int i = 0; i < mat.length; i++) {
+        System.out.println(Arrays.toString(mat[i]));
+    }
 
 }
 
@@ -867,4 +874,83 @@ int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
 
 
     return ans;
+}
+
+//https://leetcode.com/problems/set-matrix-zeroes/description/
+//matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//[[1,0,1],[0,0,0],[1,0,1]]
+//
+//void setZeroes(int[][] matrix) {
+// boolean[] rows = new boolean[matrix.length];
+// boolean[] cols = new boolean[matrix[0].length];
+//
+//    for (int row = 0; row <matrix.length ; row++) {
+//        for (int col = 0; col < matrix[row].length; col++) {
+//            if (matrix[row][col] == 0) {
+//                rows[row]=true;
+//                cols[col]=true;
+//            }
+//
+//        }
+//    }
+//
+//    for (int row = 0; row <matrix.length ; row++) {
+//        for (int col = 0; col <matrix[row].length ; col++) {
+//            if(rows[row]||cols[col]){
+//                matrix[row][col]=0;
+//            }
+//        }
+//    }
+//}
+
+void setZeroes(int[][] matrix){
+  boolean firstRowZero = false;
+  boolean firstColZero =false;
+
+    for (int row = 0; row < matrix.length; row++) {
+        for (int col = 0; col < matrix[row].length; col++) {
+            if(matrix[row][col]==0){
+
+
+            if (row==0&&col!=0) {
+                firstRowZero=true;
+            }else if(row!=0&&col==0){
+                firstColZero=true;
+            }else if(row==0&&col==0){
+                firstColZero=true;
+                firstRowZero=true;
+            }
+            }
+        }
+
+    }
+
+    for (int row = 1; row <matrix.length ; row++) {
+        for (int col = 1; col <matrix[row].length ; col++) {
+            if(matrix[row][col]==0){
+
+                    matrix[row][0]=0;
+                    matrix[0][col]=0;
+                }
+        }
+    }
+
+
+    for (int i = 1; i < matrix.length; i++) {
+        for (int j = 1; j < matrix[i].length; j++) {
+            if(matrix[i][0]==0||matrix[0][j]==0){
+                matrix[i][j]=0;
+            }
+        }
+    }
+    if(firstRowZero){
+        for (int i = 0; i < matrix[0].length; i++) {
+            matrix[0][i]=0;
+        }
+    }
+    if(firstColZero){
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][0]=0;
+        }
+    }
 }
