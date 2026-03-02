48d930fdea8add797bd0b1f838050f86bacb56d0
dilipgour <codewithdilipgour@gmail.com>
2026-03-02 23:57:48 +0530

    Day-14

--- src/arrays.java
+++ src/arrays.java
@@ -103,7 +103,15 @@ void main(){
 //    }
 //
 
-    System.out.println(Arrays.toString(productExceptSelf(new int[]{-1,1,0,-3,3})));
+//    System.out.println(Arrays.toString(productExceptSelf(new int[]{-1,1,0,-3,3})));
+
+//    System.out.println(Arrays.toString(searchRange(new int[]{5,7,7,8,8,10},5)));
+
+//    System.out.println(canJump(new int[]{3,2,1,1,4}));
+
+//    rotate(new int[]{1,2,3,4,5,6,7},3);
+
+    sortColors(new int[]{2,0,2,1,1,0});
 
 }
 
@@ -996,4 +1004,120 @@ int[] productExceptSelf(int[] nums){
     }
     ans[0]=rightProduct;
     return ans;
+}
+
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/?
+//nums = [5,7,7,8,8,10], target = 8
+//[3,4]
+
+int[] searchRange(int[] nums, int target) {
+int[] ans = new int[]{-1,-1};
+int firstIdx = binarySearch(nums,target,true);
+
+if(firstIdx==-1){
+    return ans;
+}
+int lastIdx = binarySearch(nums,target,false);
+ans[0]=firstIdx;
+ans[1]=lastIdx;
+return ans;
+}
+
+int binarySearch(int[] arr,int target,boolean isFindingFirst){
+    int start =0;
+    int end = arr.length-1;
+    int ans= -1;
+
+    while(start<=end){
+        int mid = start+(end-start)/2;
+        if(arr[mid]<target){
+            start = mid+1;
+        }else if(arr[mid]>target){
+            end=mid-1;
+
+        }else{
+            ans=mid;
+            if(isFindingFirst){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+    }
+    return ans;
+}
+
+//https://leetcode.com/problems/jump-game/
+//nums = [2,3,1,1,4]
+//true
+
+boolean canJump(int[] nums) {
+int i=0;
+int maxReach =0;
+
+while(i<nums.length){
+    if(i>maxReach) return false;
+    maxReach=Math.max(maxReach, nums[i]+i);
+    i++;
+}
+return true;
+
+}
+
+//https://leetcode.com/problems/rotate-array/description/
+//nums = [1,2,3,4,5,6,7], k = 3
+//[5,6,7,1,2,3,4]
+
+//void rotate(int[] nums, int k) {
+//    int n= nums.length;
+//  int[] ans = new int[n];
+//
+//    for (int i = 0; i <n; i++) {
+//        ans[(i+k)%n] = nums[i];
+//    }
+//
+//    System.out.println(Arrays.toString(ans));
+//
+//}
+
+void rotate(int[] nums, int k) {
+    int n= nums.length;
+    k=k%n;
+  rotate(nums,0,n-1);
+  rotate(nums,0,k-1);
+  rotate(nums,k,n-1);
+    System.out.println(Arrays.toString(nums));
+
+}
+
+void rotate(int[] arr, int s, int e){
+
+    while(s<e){
+        int temp = arr[s];
+        arr[s]= arr[e];
+        arr[e]=temp;
+        s++;
+        e--;
+    }
+}
+
+//https://leetcode.com/problems/sort-colors/
+//nums = [2,0,2,1,1,0]
+//[0,0,1,1,2,2]
+
+void sortColors(int[] nums) {
+int[] freq = new int[3];
+
+    for (int i = 0; i < nums.length; i++) {
+        freq[nums[i]]++;
+    }
+
+    int idx=0;
+    for (int i = 0; i < freq.length; i++) {
+        while(freq[i]>0){
+            nums[idx++] = i;
+            freq[i]--;
+        }
+    }
+
 }
