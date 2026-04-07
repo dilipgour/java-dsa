67477cd43867b1149c96c21badbc8dafb9d6dc50
dilipgour <codewithdilipgour@gmail.com>
2026-04-07 21:55:36 +0530

    ...

--- src/binarySearch.java
+++ src/binarySearch.java
@@ -6,10 +6,15 @@ void main() {
 //    System.out.println(nextGreatestLetter(new char[]{'a','b','j'},'z'));
 //    System.out.println(findKthPositiveBs(new int[]{2,3,4,7,11},5));
 //    System.out.println(searchInsert(new int[]{1,3,5,6},2));
-    System.out.println(peakIndexInMountainArray(new int[]{0, 2, 3, 1, 0}));
+//    System.out.println(peakIndexInMountainArray(new int[]{0, 2, 3, 1, 0}));
+//    System.out.println(countNegatives(new int[][]{{4, 3, 2, -1},
+//            {3, 2, 1, -1},
+//            {1, 1, -1, -2},
+//            {-1, -1, -2, -3}}));
+//}
 
+    System.out.println(Arrays.toString(intersectionBs(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
 }
-
 
 int sqrt(int num) {
     int start = 0;
@@ -227,4 +232,112 @@ int peakIndexInMountainArray(int[] arr) {
         }
     }
     return s;
+}
+
+//https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
+//i/p: grid = [[4,3,2,-1],
+//             [3,2,1,-1],
+//             [1,1,-1,-2],
+//             [-1,-1,-2,-3]]
+//o/p:8
+
+int countNegatives(int[][] grid) {
+    int count = 0;
+    int cols = grid[0].length;
+
+    for (int i = 0; i < grid.length; i++) {
+        int s = 0;
+        int e = cols - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (grid[i][mid] < 0) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        count += cols - s;
+    }
+    return count;
+}
+
+int countNegativeStairCase(int[][] grid) {
+    int row = 0;
+    int col = grid[0].length - 1;
+    int count = 0;
+    int rowEnd = grid.length;
+    int colEnd = 0;
+
+    while (row < rowEnd && col >= colEnd) {
+        if (grid[row][col] < 0) {
+            count += rowEnd - row;
+            col--;
+        } else {
+            row++;
+        }
+
+    }
+    return count;
+}
+
+//https://leetcode.com/problems/intersection-of-two-arrays/
+//i/p:nums1 = [1,2,2,1], nums2 = [2,2]
+//o/p:[2]
+
+int[] intersection(int[] nums1, int[] nums2) {
+    Set<Integer> ans = new HashSet<Integer>();
+
+    for (int i = 0; i < nums1.length; i++) {
+
+        for (int j = 0; j < nums2.length; j++) {
+            if (nums1[i] == nums2[j]) {
+                ans.add(nums1[i]);
+            }
+        }
+    }
+    int[] arr = new int[ans.size()];
+    int index = 0;
+
+    for (Integer value : ans) {
+        arr[index++] = value.intValue();
+    }
+    return arr;
+}
+
+int[] intersectionBs(int[] nums1, int[] nums2){
+    Arrays.sort(nums2);
+    Set<Integer> ans = new HashSet<Integer>();
+
+    for (int i = 0; i < nums1.length; i++) {
+        int idx = binarySearch(nums2,nums1[i]);
+        if(idx!=-1){
+            ans.add(nums1[i]);
+        }
+    }
+
+    int[] arr = new int[ans.size()];
+    int index = 0;
+
+    for (int value : ans) {
+        arr[index++] = value;
+    }
+    return arr;
+}
+
+int binarySearch(int[] arr, int target){
+    int s=0;
+    int e=arr.length-1;
+    
+    while(s<=e){
+        int mid = s+(e-s)/2;
+        if(arr[mid]==target) return mid;
+        if(arr[mid]<target){
+            s=mid+1;
+        }else{
+            e=mid-1;
+        }
+    }
+    return -1;
 }
